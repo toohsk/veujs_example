56b568c548df4a309d1d312f2488c9f1ddcd88cf
toohsk <toohsk@gmail.com>
2019-05-21 17:39:13 +0900

    Lesson 2.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm, fuzzy socks'
+        description: 'A pair of warm, fuzzy socks',
+        image: '../assets/vmSocks-green.png',
+        item_link: 'https://www.amazon.co.jp/s?k=%E9%9D%B4%E4%B8%8B&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&ref=nb_sb_noss_1'
     }
 })
