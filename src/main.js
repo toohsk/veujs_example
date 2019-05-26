01001191328edaaf4bb508552baf40104d8e6f69
toohsk <toohsk@gmail.com>
2019-05-26 19:07:30 +0900

    Lesson 3.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ var app = new Vue({
         product: 'Socks',
         description: 'A pair of warm, fuzzy socks',
         image: '../assets/vmSocks-green.png',
-        item_link: 'https://www.amazon.co.jp/s?k=%E9%9D%B4%E4%B8%8B&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&ref=nb_sb_noss_1'
+        item_link: 'https://www.amazon.co.jp/s?k=%E9%9D%B4%E4%B8%8B&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&ref=nb_sb_noss_1',
+        inventory: 11,
+        onSale: true
     }
 })
